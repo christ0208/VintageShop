090d7e5630bb5f6b19165b5444119e9f9a5cd037
christ0208 <christopherlimawan@gmail.com>
2019-11-29 20:43:42 +0700

    Added About Us Page (Not Done)

--- js/navbar-navigate.js
+++ js/navbar-navigate.js
@@ -1,5 +1,25 @@
+function homeButtonClick(){
+    window.location.href = "index.html";
+}
+
+function itemButtonClick(){
+    console.log('item button');
+}
+
+function furnitureButtonClick(){
+    console.log('furniture button');
+}
+
+function aboutButtonClick(){
+    window.location.href = "about.html";
+}
+
 function registerButtonClick(){
     window.location.href = "register.html";
 }
 
+$('#home-button').click(homeButtonClick);
+$('#item-button').click(itemButtonClick);
+$('#furniture-button').click(furnitureButtonClick);
+$('#about-button').click(aboutButtonClick);
 $('#register-button').click(registerButtonClick);
