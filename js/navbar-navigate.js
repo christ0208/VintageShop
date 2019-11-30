72103a8f2edf901e81e8106f7cc7044b1e975083
christ0208 <christopherlimawan@gmail.com>
2019-11-30 17:47:44 +0700

    Fixed Navigation Bar For Easy Redirection to Several Pages

--- js/navbar-navigate.js
+++ js/navbar-navigate.js
@@ -1,25 +1,9 @@
-function homeButtonClick(){
-    window.location.href = "index.html";
+function changePage(e){
+    window.location.href = redirection[e.currentTarget.id];
 }
 
-function itemButtonClick(){
-    console.log('item button');
-}
-
-function furnitureButtonClick(){
-    console.log('furniture button');
-}
-
-function aboutButtonClick(){
-    window.location.href = "about.html";
-}
-
-function registerButtonClick(){
-    window.location.href = "register.html";
-}
-
-$('#home-button').click(homeButtonClick);
-$('#item-button').click(itemButtonClick);
-$('#furniture-button').click(furnitureButtonClick);
-$('#about-button').click(aboutButtonClick);
-$('#register-button').click(registerButtonClick);
+$('#home-button').click(changePage);
+$('#item-button').click(changePage);
+$('#furniture-button').click(changePage);
+$('#about-button').click(changePage);
+$('#register-button').click(changePage);
---
+++ js/navbar-navigation-constants.js
@@ -0,0 +1,7 @@
+const redirection = {
+    "home-button": "index.html",
+    "item-button": "vintageitem.html",
+    "furniture-button": "vintagefurniture.html",
+    "about-button": "about.html",
+    "register-button": "register.html"
+}
